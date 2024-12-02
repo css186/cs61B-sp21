d686e1e76f8002e248a8c6b67cc385b979a094fe
css186 <cssrm31@gmail.com>
2024-12-02 09:51:05 -0500

    Add simple implementation of DLList to lecture code

---
+++ lecture_code/DLList/DLList.java
@@ -0,0 +1,98 @@
+package DLList;
+
+public class DLList<T> {
+
+    private static class ListNode<T> {
+        T item;
+        ListNode<T> prev;
+        ListNode<T> next;
+
+        ListNode(T i) {
+            item = i;
+            prev = null;
+            next = null;
+        }
+    }
+
+    ListNode<T> sentinel;
+    private int size;
+
+    /**
+     * Constructor
+     */
+    public DLList() {
+        sentinel = new ListNode<>(null);
+        sentinel.prev = sentinel;
+        sentinel.next = sentinel;
+        size = 0;
+    }
+
+    public void addFirst(T item) {
+        ListNode<T> newNode = new ListNode<>(item);
+        sentinel.next.prev = newNode;
+        newNode.next = sentinel.next;
+
+        newNode.prev = sentinel;
+        sentinel.next = newNode;
+        size++;
+    }
+
+    public void addLast(T item) {
+        ListNode<T> newNode = new ListNode<>(item);
+        sentinel.prev.next = newNode;
+        newNode.prev = sentinel.prev;
+
+        newNode.next = sentinel;
+        sentinel.prev = newNode;
+        size++;
+    }
+
+    public T removeFirst() {
+        if (size == 0) {
+            return null;
+        }
+        T returnItem = sentinel.next.item;
+        sentinel.next = sentinel.next.next;
+        sentinel.next.prev = sentinel;
+        size--;
+        return returnItem;
+    }
+
+    public T removeLast() {
+        if (size == 0) {
+            return null;
+        }
+        T returnItem = sentinel.prev.item;
+        sentinel.prev = sentinel.prev.prev;
+        sentinel.prev.next = sentinel;
+        size--;
+        return returnItem;
+    }
+
+    public T getLast() {
+        if (size == 0) {
+            return null;
+        }
+        return sentinel.prev.item;
+    }
+
+    public T getFrist() {
+        if (size == 0) {
+            return null;
+        }
+        return sentinel.next.item;
+    }
+
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            return null;
+        }
+        ListNode<T> curr = sentinel.next;
+
+        for (int i = 0; i < index; i++) {
+            curr = curr.next;
+        }
+        return curr.item;
+    }
+
+}
